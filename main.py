e169eea5242623df50e4511e45aa4e61ba9f426b
GSS_DOMAIN\kai_hung <kai_hung@gss.com.tw>
2023-12-08 10:54:46 +0800

    Update - now you can get the xlsx report with different dependencies

--- main.py
+++ main.py
@@ -18,22 +18,37 @@ update_request_name = config.get('diff_update_request_name') or 'update-request'
 update_request_project_name = config.get('diff_update_request_project_name') or f'diff_update_request_{today}'
 path_to_diff_file = os.path.join(diff_file_directory, update_request_name)
 
+def find_dependencies_with_action(deps, action):
+    """
+    Recursive function to find all dependencies and sub-dependencies with a specific action.
+    """
+    all_deps = []
+    for dep in deps:
+        if dep.get('action') == action:
+            all_deps.append(dep)
+        children = dep.get('children', [])
+        all_deps.extend(find_dependencies_with_action(children, action))
+    return all_deps
 
 def find_new_or_updated_dependencies(initial_deps, update_deps):
     """
-    Find new dependencies that are present in the update_deps but not in the initial_deps,
-    and find updated dependencies by comparing their versions.
+    Find new or updated dependencies and label them as 'Added' or 'Change Version'.
     """
     initial_dep_dict = {(dep.get('artifactId'), dep.get('groupId')): dep for dep in initial_deps}
-    new_or_updated_dependencies = []
-    for dep in update_deps:
-        key = (dep.get('artifactId'), dep.get('groupId'))
-        if key not in initial_dep_dict:
-            new_or_updated_dependencies.append(dep)
-        elif dep.get('version') != initial_dep_dict[key].get('version'):
-            new_or_updated_dependencies.append(dep)
-    return new_or_updated_dependencies
+    update_dep_dict = {(dep.get('artifactId'), dep.get('groupId')): dep for dep in update_deps}
 
+    # Process direct dependencies
+    for dep_key, dep in update_dep_dict.items():
+        if dep_key not in initial_dep_dict:
+            dep['action'] = 'Added'
+        elif dep.get('version') != initial_dep_dict[dep_key].get('version'):
+            dep['action'] = 'Change Version'
+
+    # Find all new or updated dependencies including sub-dependencies
+    new_or_updated_deps = find_dependencies_with_action(update_deps, 'Added') + \
+                          find_dependencies_with_action(update_deps, 'Change Version')
+
+    return new_or_updated_deps, len(initial_deps), len(update_deps)
 
 def generate_diff_file():
     """
@@ -47,7 +62,8 @@ def generate_diff_file():
         update_json = json.load(file)
 
     # Update specific fields from the update file
-    fields_to_preserve = ['aggregateModules', 'preserveModuleStructure', 'extraProperties', 'scanSummaryInfo', 'contributions']
+    fields_to_preserve = ['aggregateModules', 'preserveModuleStructure', 'extraProperties', 'scanSummaryInfo',
+                          'contributions']
     for field in fields_to_preserve:
         initial_json[field] = update_json.get(field)
 
@@ -55,7 +71,8 @@ def generate_diff_file():
     initial_deps = initial_json.get('projects')[0].get('dependencies', [])
     update_deps = update_json.get('projects')[0].get('dependencies', [])
 
-    new_or_updated_deps = find_new_or_updated_dependencies(initial_deps, update_deps)
+    new_or_updated_deps, initial_deps_count, update_deps_count = find_new_or_updated_dependencies(initial_deps,
+                                                                                                  update_deps)
 
     # Update dependencies in the initial JSON
     initial_json['projects'][0]['dependencies'] = new_or_updated_deps
@@ -69,24 +86,24 @@ def generate_diff_file():
         file.write(json.dumps(initial_json, indent=4))
     print(f"Diff file generated: {path_to_diff_file}.txt")
 
+    return initial_deps_count, update_deps_count
 
 def generate_excel_report():
     """
-    Generate an Excel report for the new or updated dependencies from the diff file.
+    Generate an Excel report for the new or updated dependencies with 'Action' information.
     """
-    # Re-open the diff file to get the new or updated dependencies
     with open(path_to_diff_file + '.txt', 'r', encoding="utf8") as file:
         diff_json = json.load(file)
 
     new_or_updated_deps = diff_json['projects'][0]['dependencies']
 
-    excel_data = [{'artifactId': dep.get('artifactId'), 'groupId': dep.get('groupId'), 'version': dep.get('version')}
+    excel_data = [{'artifactId': dep.get('artifactId'), 'groupId': dep.get('groupId'), 'version': dep.get('version'),
+                   'action': dep.get('action')}
                   for dep in new_or_updated_deps]
     df = pd.DataFrame(excel_data)
     excel_path = os.path.join(diff_file_directory, 'dependency_changes.xlsx')
     df.to_excel(excel_path, index=False)
     print(f"Excel report generated: {excel_path}")
-
 
 def check_files():
     """
@@ -100,8 +117,15 @@ def check_files():
     if not os.path.isdir(diff_file_directory):
         raise NotADirectoryError(f"The diff file directory does not exist: {diff_file_directory}")
 
-
 if __name__ == '__main__':
-    check_files()  # 檢查文件是否存在
-    generate_diff_file()  # 生成差異文件
-    generate_excel_report()  # 生成Excel報告
+    check_files()  # Check if the files exist
+    initial_count, update_count = generate_diff_file()  # Generate diff file and get library counts
+    generate_excel_report()  # Generate Excel report
+
+    # Report the change in dependencies count
+    print(f"Initial dependencies count: {initial_count}")
+    print(f"Updated dependencies count: {update_count}")
+    if initial_count != update_count:
+        print(f"Dependencies count changed. Difference: {update_count - initial_count}")
+    else:
+        print("No change in dependencies count.")
